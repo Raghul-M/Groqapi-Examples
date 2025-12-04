c556876893569779c76c04337cba80703ccea2e7
Raghul-M <raghul.m1430@gmail.com>
2025-12-04 07:01:30 +0530

    Added file uploading functionality

--- app.py
+++ app.py
@@ -9,17 +9,26 @@ client = Groq(
     api_key=os.environ.get("GROQ_API_KEY", "").strip(),  # This is the default and can be omitted
 )
 
-st.title("Groq Chat")
+st.title("Groq Chat and File Analysis Assistant")
 
 user_input = st.text_input("Enter your prompt")
+file_input = st.file_uploader("Upload a file", type=["pdf", "docx", "txt","log","json","yaml","yml"])
+if file_input:
+    file_content = file_input.read()
+    print(file_content)
+else:
+    file_content = "No file needed its an general chat"
+    print(file_content)
 if st.button("Generate"):
     chat_completion = client.chat.completions.create(
         messages=[
             {
                 "role": "user",
-                "content": user_input,
+                "content": user_input + f" file content: {file_content}"  ,
             }
         ],
         model="openai/gpt-oss-20b",
+        temperature=0.7,
+        max_tokens=1000
     )
     st.write(chat_completion.choices[0].message.content)
